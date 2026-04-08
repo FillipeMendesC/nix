aa87f3abda79b62671156d8016cf80463c26722a
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-04-08 19:03:16 -0300

    fix - BAT0 para BAT1

--- hosts/nixos/battery.nix
+++ hosts/nixos/battery.nix
@@ -8,8 +8,8 @@
   services.tlp = {
     enable = true;
     settings = {
-      START_CHARGE_THRESH_BAT0 = 75;
-      STOP_CHARGE_THRESH_BAT0 = 80;
+      START_CHARGE_THRESH_BAT1 = 75;
+      STOP_CHARGE_THRESH_BAT1 = 80;
 
       # Conectado
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
