248e7ffbede09d06f6b82c3e63512c36d65e87fc
FillipeMendesC <fillipemcoelho@hotmail.com>
2026-06-22 00:34:32 -0300

    refac - mudar configuração para deixar 5 gerações no gc

--- modules/nixos/system/nix.nix
+++ modules/nixos/system/nix.nix
@@ -12,14 +12,23 @@
       ];
       auto-optimise-store = true;
 
-      substituters = [ "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
-      trusted-substituters = [ "https://hyprland.cachix.org" "https://nix-community.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-substituters = [
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 3d";
+      options = "--delete-generations +5";
     };
 
   };
@@ -36,13 +45,13 @@
 
   security.polkit.enable = true;
 
-    programs.nix-ld = {
-      enable = true;
-      libraries = with pkgs; [
-        stdenv.cc.cc
-        zlib
-      ];
-    };
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      stdenv.cc.cc
+      zlib
+    ];
+  };
 
   hardware.enableRedistributableFirmware = true;
 
